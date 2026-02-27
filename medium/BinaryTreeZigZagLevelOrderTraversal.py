11018259605ffa58ed69331cb38611f288313566
rahulsharma558991 <rahulsharma558991@gmail.com>
2026-02-27 23:21:18 +0530

    Added code for binary tree zigzag level order traversal

--- medium/BinaryTreeZigZagLevelOrderTraversal.py
+++ medium/BinaryTreeZigZagLevelOrderTraversal.py
@@ -1,5 +1,5 @@
 '''
-Problem URL: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+Problem URL: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/
 
 Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
 
