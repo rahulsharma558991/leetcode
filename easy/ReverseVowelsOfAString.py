28d1e288c85dcddebd6ba323c7022821459f5880
rahulthetoolsmaker <252770553+rahulsharma558991@users.noreply.github.com>
2026-03-17 22:49:31 +0530

    Added code for the ReverseVowels of a string

---
+++ easy/ReverseVowelsOfAString.py
@@ -0,0 +1,67 @@
+'''
+Problem URL: https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+
+Given a string s, reverse only all the vowels in the string and return it.
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+Example 1:
+Input: s = "IceCreAm"
+Output: "AceCreIm"
+Explanation:
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+Example 2:
+Input: s = "leetcode"
+Output: "leotcede"
+
+Constraints:
+1 <= s.length <= 3 * 105
+s consist of printable ASCII characters.
+'''
+class Solution(object):
+    def reverseVowels(self, s):
+        # # Convert the input string to a character array.
+        # word = list(s)
+        # start = 0
+        # end = len(s) - 1
+        # vowels = "aeiouAEIOU"
+        
+        # # Loop until the start pointer is no longer less than the end pointer.
+        # while start < end:
+        #     # Move the start pointer towards the end until it points to a vowel.
+        #     while start < end and vowels.find(word[start]) == -1:
+        #         start += 1
+            
+        #     # Move the end pointer towards the start until it points to a vowel.
+        #     while start < end and vowels.find(word[end]) == -1:
+        #         end -= 1
+            
+        #     # Swap the vowels found at the start and end positions.
+        #     word[start], word[end] = word[end], word[start]
+            
+        #     # Move the pointers towards each other for the next iteration.
+        #     start += 1
+        #     end -= 1
+        
+        # # Convert the character array back to a string and return the result.
+        # return "".join(word)
+
+
+
+
+
+
+
+        s=list(s)
+        vowels='aeiouAEIOU'
+        left,right=0,len(s)-1
+        while(left<right):
+            if(s[left] not in vowels):
+                left+=1
+            elif(s[right] not in vowels):
+                right-=1
+            else:
+                s[left],s[right]=s[right],s[left]
+                left+=1
+                right-=1
+        return("".join(s)) 
